1549d9d4d0738c5009eafa942431239ad64a3b6d
Kenneth Colon <kennethmanuel250@gmail.com>
2020-08-22 03:29:18 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,18 @@ require 'pry'
 class String
 
   def sentence?
-    if  
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+      self.length
   end
 end
